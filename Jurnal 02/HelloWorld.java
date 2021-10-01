c317c89b79076e071c6c1b2ae1aa003e20e4bb11
Chikal Fachdiana <62524948+ChikalF@users.noreply.github.com>
2021-10-01 09:37:30 +0700

    HelloWorld.java

---
+++ Jurnal 02/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Nama: Chikal Fachdiana!");
+		System.out.println("NIM: 6706213056");
+		System.out.println("Kelas: D3RPLA-45-02");
+	}
+}
